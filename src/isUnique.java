4a10e50ede33639b85387668b5712ca872f2ba2b
Ali <afsaberi23@gmail.com>
2016-10-22 19:34:09 -0400

    Polished 1.1 (isUnique)

--- src/isUnique.java
+++ src/isUnique.java
@@ -4,22 +4,24 @@ import java.util.HashMap;
  * Created by Ali on 2016-10-16.
  */
 public class isUnique {
-
     public static void main (String [] args){
         HashMap<Character,Integer> hash=new HashMap<>();
         Boolean isUnique=true;
-        String input="helo";
-        for(int i=0;i<input.length();i++){
-            if ( (hash.get(input.charAt(i))!=null)&& hash.get(input.charAt(i))>=1){
-                hash.put(input.charAt(i),hash.get(input.charAt(i))+1);
-                isUnique=false;
+        //returns false
+        String ww="These characters are not unique";
+        //returns true;
+        String input="This col man";
+            for(int i=0;i<input.length();i++) {
+                if (input.charAt(i)==' ');
+                else if ((hash.get(input.charAt(i)) != null) && (hash.get(input.charAt(i))) >= 1) {
+                    hash.put(input.charAt(i), hash.get(input.charAt(i)) + 1);
+                    isUnique = false;
+                 }
+                else hash.put(input.charAt(i), 1);
             }
-            else{
-                hash.put(input.charAt(i),1);
-            }
-        }
         for(char i:hash.keySet())System.out.println("key: "+ i+" value:"+hash.get(i));
-            System.out.println(isUnique);
+        if(isUnique)System.out.println("\nThe characters in the string are unique");
+        else System.out.println("\nThe characters in the string are NOT unique");
 
 
     }
